53adc381606753764ecc998bd4742d7a04babd3a
Charlie May <camiv95@gmail.com>
2020-07-09 14:11:18 -0500

    Prepare to distrubte the package

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="my_code", # the name that you will install via pip
+    name="lambdata-6", # the name that you will install via pip
     version="1.0",
     author="Charlie May",
     author_email="camiv95@gmail.com",
-    description="A function that extracts year, month, and day from a date column and add each to a new column",
+    description="A short description",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
