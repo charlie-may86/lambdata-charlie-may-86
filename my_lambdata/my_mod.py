3e15d219d78de2b179853a88e54907f4140ac0eb
Charlie May <camiv95@gmail.com>
2020-07-09 22:05:35 -0500

    Pep 8 Style updates

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -12,15 +12,18 @@ def enlarge(n):
     """
     return n * 100
 
-def train_validation_test_split(X, y, train_size=0.7, val_size=0.1, test_size=0.2, random_state=None, shuffle=True):
-            
+
+def train_validation_test_split(X, y, train_size=0.7, val_size=0.1,
+                                test_size=0.2, random_state=None,
+                                shuffle=True):
+
     X_train_val, X_test, y_train_val, y_test = train_test_split(
         X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
-    
+
     X_train, X_val, y_train, y_val = train_test_split(
-        X_train_val, y_train_val, test_size=val_size/(train_size+val_size), 
+        X_train_val, y_train_val, test_size=val_size/(train_size+val_size),
         random_state=random_state, shuffle=shuffle)
-    
+
     return X_train, X_val, X_test, y_train, y_val, y_test
 
 if __name__ == "__main__":
@@ -35,4 +38,4 @@ if __name__ == "__main__":
     df['target'] = raw_data['target']
     print(df.shape)
 
-    breakpoint()
+    # breakpoint()
